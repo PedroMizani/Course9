a937b9bbb1870791bd90a7f285428d223d9cc476
PedroMizani <pedromizani@outlook.com>
2023-03-26 15:13:55 -0300

    Exercicio de Composição C# cap9

---
+++ Course9/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace Course9.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() 
+        { 
+
+        }
+
+        public Department (string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Course9/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course9.Entities.Enums
+{ 
+        enum WorkerLevel : int
+        {
+            Junior = 0,
+            MidLevel = 1,
+            Senior = 2
+        };
+
+    
+}
